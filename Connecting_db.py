09c504ca7a4b49b66f04ebcdeda9d4f014df7677
lapiceroazul4 <lapiceroazul@proton.me>
2023-08-11 10:03:17 -0500

    actualizando informacion de ingreso

--- Connecting_db.py
+++ Connecting_db.py
@@ -5,15 +5,17 @@ with open('db_config.json', 'r') as json_file:
     data = json.load(json_file)
     usuario = data["user"]
     password = data["passwd"]
+    server = data["server"]
+    database = data["database"]
 
 class connecting_database():
 
   def __init__ (self):
     self.connection = pymysql.connect(
-        host="localhost",
+        host=server,
         user=usuario,
         passwd=password,
-        db="workshop"
+        db=database
 
     )
     self.cursor = self.connection.cursor()
